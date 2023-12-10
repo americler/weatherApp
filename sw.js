03112ce5e7d84ef57b4dfcc387d6e8110ec73f29
americler <77300047+americler@users.noreply.github.com>
2023-12-10 16:50:31 +0300

    .

--- sw.js
+++ sw.js
@@ -1,13 +1,6 @@
-// This variable will save the event for later use.
-let deferredPrompt;
-window.addEventListener("beforeinstallprompt", (e) => {
-  // Prevents the default mini-infobar or install dialog from appearing on mobile
-  e.preventDefault();
-  // Save the event because you'll need to trigger it later.
-  deferredPrompt = e;
-  // Show your customized install prompt for your PWA
-  // Your own UI doesn't have to be a single element, you
-  // can have buttons in different locations, or wait to prompt
-  // as part of a critical journey.
-  showInAppInstallPromotion();
+self.addEventListener("install", (event) => {
+  console.log("Service worker installed");
+});
+self.addEventListener("activate", (event) => {
+  console.log("Service worker activated");
 });
